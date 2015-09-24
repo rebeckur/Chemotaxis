a0d6be6c514cf25e809e60d62f66d1a63c7e6fbe
unknown <releung1@697-334-Umbar.sf.k12.internal>
2015-09-24 11:35:03 -0700

    Added idle class

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -5,6 +5,11 @@ int bacColor;
 
 Bacteria [] colony;
 
+int idleX;
+int idleY;
+
+Idle [] fireflies;
+
 PImage forest;
 PImage ocean;
 
@@ -13,19 +18,25 @@ boolean backChange = true;
 void setup()   
 {     
  	//initialize bacteria variables here
-  	size(500, 320);
+  	size(500, 330);
  	frameRate(6);
 
  	forest = loadImage("nightsky.png");
  	ocean = loadImage("oceannight.jpg");
 
- 	colony = new Bacteria[150];
+ 	colony = new Bacteria[100];
  	for (int j = 0; j < colony.length; j++)
  	{
  		colony[j] = new Bacteria(100,150);
  	}
+
+ 	fireflies = new Idle[50];
+ 	for (int a = 0; a < fireflies.length; a++)
+ 	{
+ 		fireflies[a] = new Idle();
+ 	}
 }
-   
+
 void draw()   
 {    
  	background(0);
@@ -47,11 +58,18 @@ void draw()
 	 	}
 	}
  	
+	for (int b = 0; b < fireflies.length; b++)
+	{
+		fireflies[b].move();
+		fireflies[b].show();
+	}
+
  	for (int i = 0; i < colony.length; i++)
  	{
 	 	colony[i].move();
 	 	colony[i].show();
  	}
+
 } 
 
 class Bacteria    
@@ -70,19 +88,19 @@ class Bacteria
  		//biased walk towards the mouse
  		if (bacX <= pmouseX)
  		{
- 			bacX += (int)(Math.random()*20)+18;
+ 			bacX += (int)(Math.random()*20)+17;
  		}
  		if (bacX >= pmouseX)
  		{
- 			bacX += (int)(Math.random()*20)-18;
+ 			bacX += (int)(Math.random()*20)-17;
  		}
  		if (bacY <= pmouseY)
  		{
- 			bacY += (int)(Math.random()*20)+18;
+ 			bacY += (int)(Math.random()*20)+17;
  		}
  		if (bacY >= pmouseY)
  		{
- 			bacY += (int)(Math.random()*20)-18;
+ 			bacY += (int)(Math.random()*20)-17;
  		}
  		//scattering
  		if (mousePressed)
@@ -101,3 +119,25 @@ class Bacteria
  		ellipse(bacX, bacY, size, size);
  	}
 }    
+
+class Idle
+{
+	Idle()
+	{
+		idleX = (int)(Math.random()*500)+1;
+		idleY = (int)(Math.random()*321);
+	}
+
+	void move()
+	{
+		idleX += (int)(Math.random()*9)-4;
+		idleY += (int)(Math.random()*9)-4;
+	}
+	void show()
+	{
+		int size = (int)(Math.random()*5)+1;
+ 		noStroke();
+ 		fill(color(255, 255, (int)(Math.random()*150)+50, 100));
+ 		ellipse(idleX, idleY, size, size);
+	}
+}
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -21,6 +21,11 @@ int bacColor;
 
 Bacteria [] colony;
 
+int idleX;
+int idleY;
+
+Idle [] fireflies;
+
 PImage forest;
 PImage ocean;
 
@@ -29,18 +34,25 @@ boolean backChange = true;
 public void setup()   
 {     
  	//initialize bacteria variables here
-  	size(500, 320);
+  	size(500, 330);
  	frameRate(6);
 
  	forest = loadImage("nightsky.png");
  	ocean = loadImage("oceannight.jpg");
 
- 	colony = new Bacteria[150];
+ 	colony = new Bacteria[100];
  	for (int j = 0; j < colony.length; j++)
  	{
  		colony[j] = new Bacteria(100,150);
  	}
-}   
+
+ 	fireflies = new Idle[50];
+ 	for (int a = 0; a < fireflies.length; a++)
+ 	{
+ 		fireflies[a] = new Idle();
+ 	}
+}
+
 public void draw()   
 {    
  	background(0);
@@ -62,11 +74,18 @@ public void draw()
 	 	}
 	}
  	
+	for (int b = 0; b < fireflies.length; b++)
+	{
+		fireflies[b].move();
+		fireflies[b].show();
+	}
+
  	for (int i = 0; i < colony.length; i++)
  	{
 	 	colony[i].move();
 	 	colony[i].show();
  	}
+
 } 
 
 class Bacteria    
@@ -85,19 +104,19 @@ class Bacteria
  		//biased walk towards the mouse
  		if (bacX <= pmouseX)
  		{
- 			bacX += (int)(Math.random()*20)+18;
+ 			bacX += (int)(Math.random()*20)+17;
  		}
  		if (bacX >= pmouseX)
  		{
- 			bacX += (int)(Math.random()*20)-18;
+ 			bacX += (int)(Math.random()*20)-17;
  		}
  		if (bacY <= pmouseY)
  		{
- 			bacY += (int)(Math.random()*20)+18;
+ 			bacY += (int)(Math.random()*20)+17;
  		}
  		if (bacY >= pmouseY)
  		{
- 			bacY += (int)(Math.random()*20)-18;
+ 			bacY += (int)(Math.random()*20)-17;
  		}
  		//scattering
  		if (mousePressed)
@@ -116,6 +135,31 @@ class Bacteria
  		ellipse(bacX, bacY, size, size);
  	}
 }    
+
+class Idle
+{
+	Idle()
+	{
+		idleX = (int)(Math.random()*500)+1;
+		idleY = (int)(Math.random()*321);
+	}
+
+	public void move()
+	{
+		if (frameCount%2 == 0)
+		{
+			idleX += (int)(Math.random()*9)-4;
+			idleY += (int)(Math.random()*9)-4;
+		}
+	}
+	public void show()
+	{
+		int size = (int)(Math.random()*5)+1;
+ 		noStroke();
+ 		fill(color(255, 255, (int)(Math.random()*150)+50, 100));
+ 		ellipse(idleX, idleY, size, size);
+	}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Chemotaxis" };
     if (passedArgs != null) {
